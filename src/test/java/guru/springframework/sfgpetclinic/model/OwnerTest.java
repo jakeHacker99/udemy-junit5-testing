992e28b66201473233d3110516951baf3dd7e73c
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-09-18 17:38:22 +0200

    impl circleci config

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,11 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import guru.springframework.sfgpetclinic.CustomArgsProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -50,4 +52,47 @@ class OwnerTest {
         System.out.println(ownerType);
     }
 
+
+    @ParameterizedTest(name="{displayName} -[{index}] {arguments}")
+    @DisplayName("Csv Input Test")
+    @CsvSource({"FL, 1,2,4",
+            "HD, 3,2,5",
+            "RGS, 6,2,5"
+
+    })
+
+    void csvInputTest(String  stateName, int val1, int val2 ) {
+        System.out.println(stateName + " = " + val1 + " : " + val2);
+
+    }
+
+
+    @DisplayName("CSV From File Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+    void csvFromFileTest(String stateName, int val1 ) {
+        System.out.println(stateName + " = " + val1 );
+    }
+    @DisplayName("Method Provider")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("getArgs")
+
+    void fromMethodTest(String stateName, int val1 ) {
+        System.out.println(stateName + " = " + val1 );
+    }
+    static Stream<Arguments> getArgs(){
+        return Stream.of(Arguments.of("FL", 124),
+                Arguments.of("HD", 325),
+                Arguments.of("RGS", 625)
+                );
+
+    }
+
+
+    @ParameterizedTest(name="{displayName} - [{index}] {arguments} ")
+    @DisplayName("")
+    @ArgumentsSource(CustomArgsProvider.class)
+    void fromCustomProviderTest(String stateName, int val1) {
+        System.out.println(stateName + " = " + val1);
+    }
 }
